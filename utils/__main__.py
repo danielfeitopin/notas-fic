8f93c72af110b5e43ca37caa36d24bfd3e84485a
Daniel Feito Pin <danielfeitopin@protonmail.com>
2026-02-28 11:41:44 +0100

    feat(parser): Add parser to retrieve data

---
+++ utils/__main__.py
@@ -0,0 +1,48 @@
+from bs4 import BeautifulSoup
+from .parser import request_data, process_data, STUDY_CODES
+
+def main():
+
+    # Retrieve data
+    print(f"Requesting data for study code {STUDY_CODE} and year {YEAR}...")
+    html: str = request_data(STUDY_CODE, LANG).text
+
+    if html:
+
+        # Parse HTML
+        print("Data retrieved successfully. Parsing HTML...")
+        if not (soup := BeautifulSoup(html, features="html.parser")):
+            raise ValueError("Failed to parse HTML content.")
+
+        # Extract course data
+        print("Extracting course data...")
+        if not (course_data := soup.find(id=f'curse{YEAR}')):
+            print(f"Failed to extract course data for year {YEAR}.")
+
+        # Extract subject results
+        print("Extracting subject results...")
+        if not (subject_results := soup.find(id=f'detailedresults{YEAR}')):
+            print(f"Failed to extract subject results for year {YEAR}.")
+
+        # Process data
+        print("Processing data...")
+        data: dict[str, dict[str, str]] = process_data(
+            course_data, subject_results)
+        
+        print(data)
+
+
+if __name__ == "__main__":
+
+    STUDY_CODE: str = STUDY_CODES.get('GCED', '')
+    YEAR: str = '2024'
+    LANG: str = 'gl'
+
+    try:
+        main()
+    except KeyboardInterrupt:
+        print("Process interrupted by user. Exiting...")
+        exit(0)
+    except Exception as e:
+        print(f"An error occurred: {e}")
+        exit(1)
---
+++ utils/parser.py
@@ -0,0 +1,74 @@
+from bs4 import BeautifulSoup
+import requests
+from requests import Response
+
+STUDY_CODES: dict[str, str] = {
+    'GEI': '614G01V01',
+    'GCED': '614G02V01',
+    'GIA': '614G03V01',
+}
+
+
+def request_data(study_code: str, lang: str = 'gl') -> Response:
+    url: str = f"https://estudos.udc.es/{lang}/study/quality/{study_code}"
+    response: Response = requests.get(url)
+    return response
+
+
+def process_course_data(course_data: BeautifulSoup) -> dict[str, str]:
+    processed_data: dict[str, str] = {
+        'study_data': {},
+        'centre_data': {}
+    }
+
+    study_data, centre_data = course_data.find_all(class_='span5')
+
+    # Process study data
+    for i, table in enumerate(study_data.find_all('table')):
+        table_body: BeautifulSoup = table.find('tbody')
+        for row in table_body.find_all('tr'):
+            processed_data['study_data'][f'STUDY_{i}'] = row.find_all(
+                'td')[-1].text.strip()
+            i += 1
+
+    # Process centre data
+    for i, table in enumerate(centre_data.find_all('table')):
+        table_body: BeautifulSoup = table.find('tbody')
+        for row in table_body.find_all('tr'):
+            processed_data['centre_data'][f'CENTRE_{i}'] = row.find_all(
+                'td')[-1].text.strip()
+            i += 1
+
+    return processed_data
+
+
+def process_subject_results(subject_results: BeautifulSoup) -> dict[int, dict]:
+    processed_results: dict[int, list] = {}
+
+    # Process subject results
+    courses: list = subject_results.find_all(class_='span5')
+
+    for i, course in enumerate(courses):
+        processed_results[i+1] = []
+
+        results: BeautifulSoup = course.find(class_='results')
+        for subject in results.find_all('dt'):
+            subject_name: str = subject.text.strip()
+            subject_metrics: BeautifulSoup = subject.find_next_sibling('dd')
+            processed_results[i+1].append({
+                'name': subject_name,
+                'metrics': (
+                    subject_metrics.find(class_='bar-success').text.strip(),
+                    subject_metrics.find(class_='bar-warning').text.strip(),
+                    subject_metrics.find(class_='bar-info').text.strip()
+                )
+            })
+
+    return processed_results
+
+
+def process_data(study_data, subject_results) -> dict[str, dict[str, str]]:
+    data: dict[str, dict[str, str]] = {}
+    data['course_data'] = process_course_data(study_data)
+    data['subject_results'] = process_subject_results(subject_results)
+    return data
